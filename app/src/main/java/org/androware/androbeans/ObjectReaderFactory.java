d1b4c23a8e40a45b422d786418e7e33eba627732
John Kirkley <john.fredrick.kirkley@gmail.com>
2016-06-25 04:22:20 -0400

    added input stream functionality to read factory

--- app/src/main/java/org/androware/androbeans/ObjectReaderFactory.java
+++ app/src/main/java/org/androware/androbeans/ObjectReaderFactory.java
@@ -2,11 +2,10 @@ package org.androware.androbeans;
 
 import android.app.Activity;
 
-import org.androware.androbeans.beans.Flow;
-import org.androware.androbeans.beans.Top;
 import org.androware.androbeans.utils.ResourceUtils;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -45,7 +44,7 @@ public class ObjectReaderFactory {
     public JsonObjectReader makeJsonReader(String resourceName, String resourceGroup, Class type, List<ObjectReadListener> objectReadListeners) throws ObjectReadException {
         JsonObjectReader jsonObjectReader = null;
         try {
-            jsonObjectReader = new JsonObjectReader(ResourceUtils.getResourceInputStream(activity, resourceName, resourceGroup), Flow.class);
+            jsonObjectReader = new JsonObjectReader(ResourceUtils.getResourceInputStream(activity, resourceName, resourceGroup), type);
             if(objectReadListeners != null) {
                 jsonObjectReader.setObjectReadListeners(objectReadListeners);
             }
@@ -69,6 +68,32 @@ public class ObjectReaderFactory {
         List<ObjectReadListener> readListeners = new ArrayList<>();
         readListeners.add(new LinkObjectReadListener());
         return makeAndRunJsonReader(resourceName, type, readListeners);
+    }
+
+    public JsonObjectReader makeJsonReader(InputStream inputStream, Class type, List<ObjectReadListener> objectReadListeners) throws ObjectReadException {
+        JsonObjectReader jsonObjectReader = null;
+        try {
+            jsonObjectReader = new JsonObjectReader(inputStream, type);
+            if(objectReadListeners != null) {
+                jsonObjectReader.setObjectReadListeners(objectReadListeners);
+            }
+            return jsonObjectReader;
+        } catch (IOException e) {
+            throw new ObjectReadException(e);
+        }
+    }
+
+
+    public Object makeAndRunJsonReader(InputStream inputStream, Class type, List<ObjectReadListener> objectReadListeners) throws ObjectReadException {
+
+        JsonObjectReader JsonObjectReader = makeJsonReader(inputStream, type, objectReadListeners);
+        return JsonObjectReader.read();
+
+    }
+    public Object makeAndRunLinkedJsonReader(InputStream inputStream, Class type) throws ObjectReadException {
+        List<ObjectReadListener> readListeners = new ArrayList<>();
+        readListeners.add(new LinkObjectReadListener());
+        return makeAndRunJsonReader(inputStream, type, readListeners);
     }
 
 }
