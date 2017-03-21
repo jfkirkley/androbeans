0a08f403d27375ff5ff5c2ddf8181e666ea7a3da
John Kirkley <jkirkley@mino.fino>
2017-03-22 00:47:26 +0700

    added reverse iterator

---
+++ app/src/main/java/org/androware/androbeans/utils/Reversed.java
@@ -0,0 +1,42 @@
+package org.androware.androbeans.utils;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+/**
+ * Created by jkirkley on 3/19/17.
+ */
+
+
+public class Reversed<T> implements Iterable<T> {
+    private final List<T> original;
+
+    public Reversed(List<T> original) {
+        this.original = original;
+    }
+
+    public Iterator<T> iterator() {
+        final ListIterator<T> i = original.listIterator(original.size());
+
+        return new Iterator<T>() {
+            public boolean hasNext() {
+                return i.hasPrevious();
+            }
+
+            public T next() {
+                return i.previous();
+            }
+
+            public void remove() {
+                i.remove();
+            }
+        };
+    }
+
+    public static <T> Reversed<T> reversed(List<T> original) {
+        return new Reversed<T>(original);
+    }
+}
+
+
